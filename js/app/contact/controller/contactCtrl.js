04166ded3170ddb7de51723cc2ba92473b6d4a83
demousermkt <demousermkt@gmail.com>
2020-12-03 11:31:52 +0530

    'GTM'

--- js/app/contact/controller/contactCtrl.js
+++ js/app/contact/controller/contactCtrl.js
@@ -2,4 +2,5 @@ var contactModule = angular.module('dc.contact');
 
  contactModule.controller('contactCtrl',['$scope','$state', '$http','$rootScope','$interval',
     function($scope,$state,$http,$rootScope,$interval){
+		$scope.selectedTab = 0;
     }]);
